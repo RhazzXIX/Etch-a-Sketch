fa2e8e8dc2f092063e899402d3ac44d201570f75
RhazzXIX <jm21tonio@gmail.com>
2022-11-14 14:38:01 +0800

    Add RGB function and restructure UI buttons
    Added function for random color generator.
    Revised drawing function to edit grid into random color.
    Restructure UI buttons to query size to user and add Darken mode.

--- javascript.js
+++ javascript.js
@@ -1,75 +1,130 @@
 const sketchbox = document.querySelector('#sketchbox');
-const x30 = document.querySelector('#x30');
-const x60 = document.querySelector('#x60');
-const x100 = document.querySelector('#x100');
+const size = document.querySelector('#size');
 const body = document.querySelector('body');
+const black = document.querySelector('#black');
+const eraser = document.querySelector('#eraser');
+const rgb = document.querySelector('#rgb')
+const clear = document.querySelector('#clear');
 
 //variable to draw
 let draw = false;
-
+let randomColor = false;
 let num = 0;
 
-x30.addEventListener('click', () => {
-    unsketch();
-    num = 30;
-    sketch();
+
+size.addEventListener('click', () => {
+    removeCanvass();
+    num = prompt('Enter a number from 10 to 100', '');
+    createCanvass();
 });
 
-x60.addEventListener('click', () => {
-    unsketch();
-    num = 60;
-    sketch();
-});
-
-x100.addEventListener('click', () => {
-    unsketch();
-    num = 100;
-    sketch();
-});
 
 //function to create grid;
-function sketch(){
-    console.log(num);
-    let size = 500/num;
-    let grid = num*num;
-    for(let i = 0; i < grid; i++) {
-        const box = document.createElement('div');
-        box.style.height = `${size}px`;
-        box.style.width = `${size}px`;
-        box.classList.add('box');
-        sketchbox.appendChild(box);
-    }
-}
+function createCanvass(){
+    if (num === null) {
+        return alert('Thanks! Come back again!');
+     } else if (num >= 10 && num <= 100) {
+         let size = 500/num;
+         let grid = num*num;
+         for(let i = 0; i < grid; i++) {
+             let div = document.createElement('div');
+             div.style.height = `${size}px`;
+             div.style.width = `${size}px`;
+             div.classList.add('box');
+             sketchbox.appendChild(div);
+         }
+     } else {
+         num = prompt('Numbers from 10 to 100 only', '');
+         createCanvass(num);
+     }
+ }
 
 
 //function to remove grid;
-function unsketch(){
+function removeCanvass(){
     const boxes = document.querySelectorAll('.box');
     for(let i = 0; i < boxes.length; i++){
     sketchbox.removeChild(boxes[i]);
     }
 }
 
-//function to draw
+//event to draw
 sketchbox.addEventListener('mousedown', function (e) {
     draw = true;
-    color();
+    drawing();
     
 }, true);
 
 body.addEventListener('mouseup', function (e) {
     draw = false;
-    console.log(draw);
+    e.stopPropagation();
 }, true);
 
-//function to change color
-function color() {
+//function for drawing
+function drawing() {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((div) => {
-        div.addEventListener('mouseover', (e) => {
-            if (draw == true) {
-                e.target.style.backgroundColor = 'black' }
+        div.addEventListener('mousedown', (e) => {
+            if (draw == true && randomColor == true) {
+                r = createRandomColor();
+                g = createRandomColor();
+                b = createRandomColor();
+                penColor = `rgb(${r}, ${g}, ${b})`;
+            e.target.style.backgroundColor = `${penColor}` 
+            } else if (draw == true) {
+                e.target.style.backgroundColor = `${penColor}` }
+                e.stopPropagation();
         });
+        div.addEventListener('mouseover', (e) => {
+            if (draw == true && randomColor == true) {
+                    r = createRandomColor();
+                    g = createRandomColor();
+                    b = createRandomColor();
+                    penColor = `rgb(${r}, ${g}, ${b})`;
+                e.target.style.backgroundColor = `${penColor}` 
+            } else if (draw == true) {
+                e.target.style.backgroundColor = `${penColor}` }
+                e.stopPropagation();
+        }); 
+    });
+}
+
+
+//function to change Color
+
+let penColor = 'black'
+
+black.addEventListener('click', (e) =>{
+    penColor = 'black';
+    randomColor = false;
+    e.stopPropagation();
+});
+
+eraser.addEventListener('click', (e) => {
+    penColor = 'white';
+    randomColor = false;
+    e.stopPropagation();
+});
+
+rgb.addEventListener('click', (e) =>{
+    randomColor = true;
+    e.stopPropagation();
+})
+
+//function for random color
+function createRandomColor () {
+   return Math.floor(Math.random()*256);
+}
+
+
+//function for Clear
+
+clear.addEventListener('click', clean);
+
+function clean() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((div) => {
+        div.style.backgroundColor = "white"
     });
 }
 
